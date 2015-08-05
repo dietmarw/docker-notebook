4b19ea596b16a3bb7d2871d1ea4520b16aeb6a50
Dietmar Winkler <dietmar.winkler@dwe.no>
2015-08-05 15:46:07 +0200

    Add pull dependency dietmarw/notebook

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@ TOKEN=`head -c 30 /dev/urandom | xxd -p`
 
 docker rm  -f  `docker ps -aq`
 
+docker pull dietmarw/notebook
+
 docker run --net=host -d -e CONFIGPROXY_AUTH_TOKEN=$TOKEN \
        --name=proxy jupyter/configurable-http-proxy \
        --default-target http://127.0.0.1:9999
